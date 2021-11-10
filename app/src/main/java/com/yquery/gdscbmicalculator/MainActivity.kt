a16ea9aef52dd533431ad0551ba56a9a098c11cb
Youssef Moataz <yousefcrunshy03@gmail.com>
2021-11-10 17:44:30 +0200

    Release.

--- app/src/main/java/com/yquery/gdscbmicalculator/MainActivity.kt
+++ app/src/main/java/com/yquery/gdscbmicalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -29,7 +32,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_input, R.id.navigation_result, R.id.navigation_about
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/yquery/gdscbmicalculator/ui/about/AboutFragment.kt
+++ app/src/main/java/com/yquery/gdscbmicalculator/ui/about/AboutFragment.kt
@@ -1,5 +1,7 @@
 package com.yquery.gdscbmicalculator.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +26,11 @@ class AboutFragment : Fragment() {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.otherAppsButton.setOnClickListener {
+            val openMyPlayStoreIntent = Intent(Intent.ACTION_VIEW)
+            openMyPlayStoreIntent.data = Uri.parse("https://play.google.com/store/apps/dev?id=6245006738668751785")
+            startActivity(openMyPlayStoreIntent)
+        }
 
         return root
     }
